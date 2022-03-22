6e330e2f94d52ab2795c9b482ae04fb55af3e438
Arisa Okasaka <68284764+arisaokasaka@users.noreply.github.com>
2022-03-22 23:38:25 +0900

    get all article and 1 article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,10 @@
 class ArticlesController < ApplicationController
   def index
+    # @で始まる変数をインスタンス変数と呼ぶ
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
   end
 end
